3f1f7f3a32a4e471fccd7bcc7dc81619cc255e36
Pruthvik <Pruthvik6666@gmail.com>
2024-12-30 22:32:14 +0530

    secret

--- week-02/chainguard/src/app/secret/page.tsx
+++ week-02/chainguard/src/app/secret/page.tsx
@@ -1,8 +1,11 @@
+import Secret from '@/components/Secret'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Secret/>
+    </div>
   )
 }
 
---
+++ week-02/chainguard/src/components/Secret.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { generateMnemonic } from "bip39";
+import React, { useState, useEffect } from "react";
+
+const Secret = () => {
+  return (
+    <div> secret</div>
+  )
+}
+
+
+export default Secret;
